a0b41165d89a84a0c4aeb4585efb47104c2e37cf
Adel Abdulazeem <adelnasr029@gmail.com>
2024-02-03 19:11:32 +0200

    ccreated weather api

---
+++ js/main.js
@@ -0,0 +1,24 @@
+//Example fetch using pokemonapi.co
+document.querySelector('button').addEventListener('click', getRandomPic)
+
+function getRandomPic(){
+    const url = 'http://api.weatherapi.com/v1/current.json?key=73709cdbefb0492baed155524240202&q=banha&aqi=yes'
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        document.querySelector('h1').innerText = data.location.name
+        document.querySelector('h2').innerText = `${data.current.temp_c} C`
+        document.querySelector('h3').innerText = data.current.condition.text
+        document.querySelector('h4').innerText = data.location.localtime
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    })
+
+}
+
+
+
+   
